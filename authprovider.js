785ccc5501c2bb45de5a93963fc8ac1e9f103081
hanhan0914 <amy8611020923@gmail.com>
2023-08-14 01:00:58 +0800

    Updates

--- authprovider.js
+++
@@ -1,5 +0,0 @@
-// import {useReducer} from 'react';
-//react 小寫 非大寫 會error
-
-// 初始值為 null
-// export const AuthContext = createContext(null);
--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-//應用的全域樣式文件
-import './index.css';
-// 從app.js引入app 引入根組件 是一切組件化渲染的開始，從該組件開始往外擴展
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
